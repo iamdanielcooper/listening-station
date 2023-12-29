aa8af1feb1c947e13ada724037a0bbfae694c4f4
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2023-12-29 22:04:36 +0000

    increase gap

--- display/display.py
+++ display/display.py
@@ -34,7 +34,7 @@ def display_album_data(albums):
     draw = ImageDraw.Draw(Himage)
 
     for index, album in enumerate(albums):
-        draw.text((10, 10 * index), display_formatted_album_information(index, album), font = font24, fill = 0)
+        draw.text((10, 30 * index), display_formatted_album_information(index, album), font = font24, fill = 0)
 
 
 
